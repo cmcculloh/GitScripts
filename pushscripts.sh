cb54c452af8de5af71554b68a8ac73adc5b824f9
Christopher McCulloh <cmcculloh@finishline.com>
2010-12-09 13:21:24 -0500

    auto commit: adjusted pushscripts file to allow for auto commits

--- pushscripts.sh
+++ pushscripts.sh
@@ -1,3 +1,9 @@
 cd ${gitscripts_path}
+
 git pull origin master
+
+git add -A
+git commit -m "auto commit: $1"
+
+
 git push origin master
--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -1,10 +1,10 @@
-source environment_template
+source "${gitscripts_path}environment_template"
 
-cat environment_template bash_profile_template > bash_profile
+cat "${gitscripts_path}environment_template" "${gitscripts_path}bash_profile_template" > "${gitscripts_path}bash_profile"
 
 cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
 cp "${gitscripts_path}motd" "${git_install}motd"
 
-rm bash_profile
+rm "${gitscripts_path}bash_profile"
 
 source "${git_install}bash_profile"
