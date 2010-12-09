25e04ab9fa8a3cd2ba0f6cad552814552a4ca09f
Christopher McCulloh <cmcculloh@finishline.com>
2010-12-09 11:14:52 -0500

    updated motd and bash_profile

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -17,9 +17,11 @@ if [ "$command" = "fromlocal" -o "$command" = "togit" ]
 	then
 	cp "${git_install}environment.sh" "${gitscripts_path}environment.sh"
 	cp "${git_install}bash_profile" "${gitscripts_path}bash_profile"
+	cp "${git_install}motd" "${gitscripts_path}motd"
 else
 	cp "${gitscripts_path}environment.sh" "${git_install}environment.sh"
 	cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
+	cp "${gitscripts_path}motd" "${git_install}motd"
 fi
 
 source "${git_install}bash_profile"
