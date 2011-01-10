62b2cfe964d4b5dc9a00e681f119f21bcda02a7e
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-10 14:31:52 -0500

    cleaning up ruby script

--- rb/checkfordiff.rb
+++ rb/checkfordiff.rb
@@ -4,13 +4,14 @@ include FileUtils
 puts <<-eot
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-______              __     _______      __     _______          __
+.______              __     _______      __     _______          __
 /_  __/_______ _____/ /__  / ___/ /__ __/ /    / ___/ / ___ ____/ /_____ ____
-/ /  / __/ _ `/ __/  '_/ / /__/ // // / _ \\  / /__/ _ | -_) __/  '_/ -_) __/
+./ /  / __/ _ `/ __/  '_/ / /__/ // // / _ \\  / /__/ _ | -_) __/  '_/ -_) __/
 /_/  /_/  \\_,_/\\__/_/\\_\\  \\___/_/ \\_,_/_.__/  \\___/_//_|__/\\__/_/\\_\\\\__/_/
 
 ================================================================================
 eot
+
 
 puts ""
 puts "=========================================================================="
@@ -23,7 +24,6 @@ puts "..........................................................................
 puts "moving into nike-track-club repo"
 #move into the Nike Track Club repository
 Dir.chdir("/workspaces/nike-track-club")
-#Dir.chdir("/workspaces/fl_git/nike-track-club")
 puts "moved into nike-track-club repo"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -53,11 +53,12 @@ puts "ran git status"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
 
+
 puts ".........................................................................."
 puts "copying files down from server"
 #copy files down from 102
-system( "scp -r dynuser@172.17.2.102:/opt/jboss/server/04finishline/deploy/finishline.ear/web-app.war/global/promos/nike-track-club/* /workspaces/nike-track-club/" )
-#copy_files = system( "scp -r dynuser@172.17.2.102:/opt/jboss/server/04finishline/deploy/finishline.ear/web-app.war/global/promos/nike-track-club/* /d/workspaces/fl_git/nike-track-club/" )
+#system( "scp -r dynuser@172.17.2.102:/opt/jboss/server/04finishline/deploy/finishline.ear/web-app.war/global/promos/nike-track-club/* /workspaces/nike-track-club/" )
+#system( "scp -r dynuser@172.17.2.102:/opt/jboss/server/03finishline/deploy/finishline.ear/web-app.war/global/promos/nike-track-club/* /d/workspaces/nike-track-club/" )
 puts "copied files down from server"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -107,7 +108,6 @@ puts "..........................................................................
 puts "moving into finishline repo"
 #Move into the finishline repository
 Dir.chdir("/workspaces/nike-track-club-fl/")
-#Dir.chdir("/workspaces/fl_git/finishline/")
 puts "moved into finishline repo"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -122,7 +122,7 @@ puts ""
 
 puts ".........................................................................."
 puts "fetching"
-fetch = system( "git fetch" )
+fetch = system( "git fetch --all" )
 puts "fetched"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -154,14 +154,22 @@ puts ""
 puts ".........................................................................."
 puts "running regex on git status to determine if changes occured"
 #determine if changes occurred.
-regex = Regexp.new(/nothing to commit \(working directory clean\)/);
+regex = Regexp.new(/nothing to commit \(working directory clean\)/)
+
+stagematchNTCdata = false
 stagematchdata = regex.match(stage_status)
 stagematches = false
+
 if stagematchdata
 	puts "STAGE MATCHES LITMUS BRANCH"
 	stagematches = true
 else
 	puts "STAGE DOES NOT MATCH LITMUS BRANCH"
+	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
+	prodmatchNTCdata = regexNTC.match(stage_status)
+	if prodmatchNTCdata
+		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
+	end
 end
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -175,9 +183,9 @@ puts ""
 
 puts ".........................................................................."
 #Pull stage to make sure it is up to date with last prod deploy
-puts "pulling fl stage"
-pull_stage = system( "git pull fl master" )
-puts "pulled stage"
+puts "pulling fl master"
+pull_master = system( "git pull fl master" )
+puts "pulled master"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
 
@@ -185,7 +193,7 @@ puts "..........................................................................
 puts "running git status"
 system( "git status ")
 #look for changes
-stage_status = `git status`
+master_status = `git status`
 puts "ran git status"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -193,14 +201,21 @@ puts ""
 puts ".........................................................................."
 puts "running regex on git status to determine if changes occured"
 #determine if changes occurred.
-regex = Regexp.new(/nothing to commit \(working directory clean\)/);
-prodmatchdata = regex.match(stage_status)
+regex = Regexp.new(/nothing to commit \(working directory clean\)/)
+prodmatchdata = regex.match(master_status)
+
 prodmatches = false
+prodmatchNTCdata = false
 if prodmatchdata
 	puts "PROD MATCHES LITMUS BRANCH"
 	prodmatches = true
 else
 	puts "PROD DOES NOT MATCH LITMUS BRANCH"
+	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
+	prodmatchNTCdata = regexNTC.match(master_status)
+	if prodmatchNTCdata
+		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
+	end
 end
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -211,9 +226,44 @@ puts "sending email"
 if stagematches && prodmatches
 	`echo "Nike Track Club has probably not reverted" | mail -s "[NTC] Nike Track Club OK" cmcculloh@finishline.com`
 else
-	`echo "Nike Track Club MIGHT HAVE REVERTED ON PROD" | mail -s "[NTC] Nike Track Club MIGHT HAVE REVERTED!!!!" cmcculloh@finishline.com`
+	if prodmatchNTCdata || stagematchNTCdata
+		`echo "Nike Track Club MIGHT HAVE REVERTED ON PROD. Some files having to do with nike, track, club or nike-track-club are different on stage or master than they are in the Litmus branch" | mail -s "[NTC] Nike Track Club MIGHT HAVE REVERTED!!!!" cmcculloh@finishline.com`
+	end
+	`echo "Nike Track Club check failed. Either stage or master do not match Litmus branch, but changes appear to be unrelated to Nike Track Club" | mail -s "[NTC] Nike Track Club Litmus branch does not match stage/master" cmcculloh@finishline.com`
 end
 puts "sent email"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+
+if matchdata
+	puts "Files on 201 match local cache of 201"
+else
+	puts "Changes were made on 201!"
+end
+
+if stagematchdata
+	puts "STAGE MATCHES LITMUS BRANCH"
+else
+	puts "STAGE DOES NOT MATCH LITMUS BRANCH"
+	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
+	prodmatchNTCdata = regexNTC.match(stage_status)
+	if prodmatchNTCdata
+		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
+	end
+end
+
+
+if prodmatchdata
+	puts "PROD MATCHES LITMUS BRANCH"
+else
+	puts "PROD DOES NOT MATCH LITMUS BRANCH"
+	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
+	prodmatchNTCdata = regexNTC.match(master_status)
+	if prodmatchNTCdata
+		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
+	end
+end
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
 
