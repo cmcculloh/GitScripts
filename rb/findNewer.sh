2a8cff17043a5b200bb078f6a8a1d065bb1b59fe
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-10 15:37:37 -0500

    updated date since microsite was updated today

--- rb/findNewer.sh
+++ rb/findNewer.sh
@@ -1,2 +1,2 @@
-touch --date "2011-01-07" /tmp/foo
+touch --date "2011-01-10" /tmp/foo
 find /opt/jboss/server/03finishline/deploy/finishline.ear/web-app.war/global/promos/nike-track-club/ -newer /tmp/foo | mail -s "[NTC] These files have changed on 102" cmcculloh@finishline.com
