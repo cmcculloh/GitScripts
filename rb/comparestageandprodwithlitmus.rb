cad3a07f03bcc5fc5d4f6edf388932d5f3942147
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-13 13:02:58 -0500

    made the script actually work

--- rb/comparestageandprodwithlitmus.rb
+++ rb/comparestageandprodwithlitmus.rb
@@ -15,16 +15,16 @@ eot
 
 puts ""
 puts "=========================================================================="
-puts "Comparing Litmus branch with stage and master"
+puts "Pulling latest version of litmus branch"
 puts "=========================================================================="
 puts ""
 
 
 puts ".........................................................................."
-puts "moving into finishline repo"
+puts "moving into litmus repo"
 #Move into the finishline repository
-Dir.chdir("/workspaces/nike-track-club-fl/")
-puts "moved into finishline repo"
+Dir.chdir("/workspaces/litmus-fl/")
+puts "moved into litmus repo"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
 
@@ -52,8 +52,113 @@ puts ""
 
 puts ".........................................................................."
 #Pull stage to make sure it is up to date with last prod deploy
+puts "pulling fl nike-track-club-litmus"
+pull_stage = system( "git pull fl nike-track-club-litmus" )
+puts "pulled nike-track-club-litmus"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+puts "running git status"
+system( "git status ")
+#look for changes
+stage_status = `git status`
+puts "ran git status"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ""
+puts "=========================================================================="
+puts "Pulling latest version of master, to compare to litmus"
+puts "=========================================================================="
+puts ""
+
+
+puts ".........................................................................."
+puts "moving into compare repo"
+#Move into the finishline repository
+Dir.chdir("/workspaces/nike-track-club-to-compare/")
+puts "moved into compare repo"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+#check out stage
+puts "checking out master"
+checkout = system( "git checkout master" )
+puts "checked out master"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+puts "resetting"
+reset = system( "git reset --hard" )
+puts "reset"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+puts "fetching"
+fetch = system( "git fetch --all" )
+puts "fetched"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+#Pull stage to make sure it is up to date with last prod deploy
+puts "pulling origin master"
+pull_master = system( "git pull origin master" )
+puts "pulled origin master"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+puts "running git status"
+system( "git status ")
+master_status = `git status`
+puts "ran git status"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+
+puts ".........................................................................."
+puts "running diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/"
+#look for changes
+master_diff = `diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/`
+puts master_diff
+puts "ran diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/"
+
+#determine if changes occurred.
+mastermatch = false
+
+if master_diff.length == 0
+	puts "MASTER MATCHES LITMUS BRANCH"
+	mastermatch = true
+else
+	puts "MASTER DOES NOT MATCH LITMUS BRANCH"
+	mastermatch = false
+end
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+
+puts ""
+puts "=========================================================================="
+puts "Pulling latest version of stage, to compare to litmus"
+puts "=========================================================================="
+puts ""
+
+puts ".........................................................................."
+puts "resetting"
+reset = system( "git reset --hard" )
+puts "reset"
+puts "``````````````````````````````````````````````````````````````````````````"
+puts ""
+
+puts ".........................................................................."
+#Pull stage to make sure it is up to date with last prod deploy
 puts "pulling fl stage"
-pull_stage = system( "git pull fl stage" )
+pull_master = system( "git pull origin stage" )
 puts "pulled stage"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -68,70 +173,21 @@ puts "``````````````````````````````````````````````````````````````````````````
 puts ""
 
 puts ".........................................................................."
-puts "running regex on git status to determine if changes occured"
+puts "running diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/"
+#look for changes
+stage_diff = `diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/`
+puts stage_diff
+puts "ran diff -r /d/workspaces/nike-track-club-to-compare/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/ /d/workspaces/litmus-fl/modules/base/j2ee-apps/base/web-app.war/global/promos/nike-track-club/"
+
 #determine if changes occurred.
-regex = Regexp.new(/nothing to commit \(working directory clean\)/)
+stagematch = false
 
-stagematchNTCdata = false
-stagematchdata = regex.match(stage_status)
-stagematches = false
-
-if stagematchdata
+if stage_diff.length == 0
 	puts "STAGE MATCHES LITMUS BRANCH"
-	stagematches = true
+	stagematch = true
 else
 	puts "STAGE DOES NOT MATCH LITMUS BRANCH"
-	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
-	prodmatchNTCdata = regexNTC.match(stage_status)
-	if prodmatchNTCdata
-		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
-	end
-end
-puts "``````````````````````````````````````````````````````````````````````````"
-puts ""
-
-puts ".........................................................................."
-puts "resetting"
-reset = system( "git reset --hard" )
-puts "reset"
-puts "``````````````````````````````````````````````````````````````````````````"
-puts ""
-
-puts ".........................................................................."
-#Pull stage to make sure it is up to date with last prod deploy
-puts "pulling fl master"
-pull_master = system( "git pull fl master" )
-puts "pulled master"
-puts "``````````````````````````````````````````````````````````````````````````"
-puts ""
-
-puts ".........................................................................."
-puts "running git status"
-system( "git status ")
-#look for changes
-master_status = `git status`
-puts "ran git status"
-puts "``````````````````````````````````````````````````````````````````````````"
-puts ""
-
-puts ".........................................................................."
-puts "running regex on git status to determine if changes occured"
-#determine if changes occurred.
-regex = Regexp.new(/nothing to commit \(working directory clean\)/)
-prodmatchdata = regex.match(master_status)
-
-prodmatches = false
-prodmatchNTCdata = false
-if prodmatchdata
-	puts "PROD MATCHES LITMUS BRANCH"
-	prodmatches = true
-else
-	puts "PROD DOES NOT MATCH LITMUS BRANCH"
-	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
-	prodmatchNTCdata = regexNTC.match(master_status)
-	if prodmatchNTCdata
-		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
-	end
+	stagematch = false
 end
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
@@ -139,40 +195,30 @@ puts ""
 puts ".........................................................................."
 puts "sending email"
 #send e-mail indicating whether changes occured
-if stagematches && prodmatches
+if stagematch && mastermatch
 	`echo "Nike Track Club has probably not reverted" | mail -s "[NTC] Nike Track Club OK" cmcculloh@finishline.com`
 else
-	if prodmatchNTCdata || stagematchNTCdata
-		`echo "Nike Track Club MIGHT HAVE REVERTED ON PROD. Some files having to do with nike, track, club or nike-track-club are different on stage or master than they are in the Litmus branch" | mail -s "[NTC] Nike Track Club MIGHT HAVE REVERTED!!!!" cmcculloh@finishline.com`
+	if stagematch
+		`echo "Nike Track Club MIGHT HAVE REVERTED ON PROD. Some files having to do with nike, track, club or nike-track-club are different on master, but not stage, than the files in the Litmus branch" | mail -s "[NTC] Nike Track Club MIGHT HAVE REVERTED!!!!" cmcculloh@finishline.com`
 	end
-	`echo "Nike Track Club check failed. Either stage or master do not match Litmus branch, but changes appear to be unrelated to Nike Track Club" | mail -s "[NTC] Nike Track Club Litmus branch does not match stage/master" cmcculloh@finishline.com`
+	`echo "Nike Track Club check failed. Stage (and possibly master) does not match Litmus branch" | mail -s "[NTC] Nike Track Club Litmus branch does not match stage/master" cmcculloh@finishline.com`
 end
 puts "sent email"
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
 
 
-if stagematchdata
+if stagematch
 	puts "STAGE MATCHES LITMUS BRANCH"
 else
 	puts "STAGE DOES NOT MATCH LITMUS BRANCH"
-	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
-	prodmatchNTCdata = regexNTC.match(stage_status)
-	if prodmatchNTCdata
-		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
-	end
 end
 
 
-if prodmatchdata
-	puts "PROD MATCHES LITMUS BRANCH"
+if mastermatch
+	puts "MASTER MATCHES LITMUS BRANCH"
 else
-	puts "PROD DOES NOT MATCH LITMUS BRANCH"
-	regexNTC = Regexp.new(/(nike\-track\-club)||(nike)||(track)||(club)/)
-	prodmatchNTCdata = regexNTC.match(master_status)
-	if prodmatchNTCdata
-		puts "PROD HAS CHANGES TO A NIKE FILE, POSSIBLY NIKE-TRACK-CLUB"
-	end
+	puts "MASTER DOES NOT MATCH LITMUS BRANCH"
 end
 puts "``````````````````````````````````````````````````````````````````````````"
 puts ""
