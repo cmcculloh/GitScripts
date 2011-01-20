6f382fe080fff4b3ccc180ab33f610a36e21889d
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-20 17:25:25 -0500

    (master) clarified how environment_template stuff worked by renaming file. Made default build into bash_profile before overrides

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -1,6 +1,6 @@
 source "${gitscripts_path}environment_template"
 
-cat "${gitscripts_path}environment_template" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_template" > "${gitscripts_path}bash_profile"
+cat "${gitscripts_path}environment_template.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_template.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_template" > "${gitscripts_path}bash_profile"
 
 cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
 cp "${gitscripts_path}motd" "${git_install}motd"
