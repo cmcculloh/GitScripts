2f0a117701ff8c085e22a26866dda8ef494cf6e1
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-21 11:05:47 -0500

    auto commit: renamed files from _template to _config and updated refresh_bash_profile.sh to match

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -1,7 +1,7 @@
-source "${gitscripts_path}environment_template.default"
-source "${gitscripts_path}environment_template.overrides"
+source "${gitscripts_path}environment_config.default"
+source "${gitscripts_path}environment_config.overrides"
 
-cat "${gitscripts_path}environment_template.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_template.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_template" > "${gitscripts_path}bash_profile"
+cat "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" > "${gitscripts_path}bash_profile"
 
 cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
 cp "${gitscripts_path}motd" "${git_install}motd"
