0dbcb017001c36ba3b41c4eca80ad879b4828eac
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-21 10:34:48 -0500

    auto commit: added a git status to pushscripts and a fetch to pullscripts

--- pullscripts.sh
+++ pullscripts.sh
@@ -1,3 +1,4 @@
 cd ${gitscripts_path}
+git fetch --all --prune
 git pull origin master
 source refresh_bash_profile.sh
--- pushscripts.sh
+++ pushscripts.sh
@@ -2,8 +2,10 @@ cd ${gitscripts_path}
 
 git pull origin master
 
+git status
 git add -A
+git status
 git commit -m "auto commit: $1"
-
+git status
 
 git push origin master
