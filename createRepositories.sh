a6538a12e3cbc7987d6ceea3a964b39f06a0db35
Christopher McCulloh <cmcculloh@finishline.com>
2011-01-25 10:09:32 -0500

    auto commit: made create repositories accept as argumentsthe users name and email as well as returning you to the location you were when you started the script

--- createRepositories.sh
+++ createRepositories.sh
@@ -1,13 +1,24 @@
+#!/bin/sh
+
+current_location=$(pwd)
+
+location="/d/workspaces/helios_workspace/"
+if [ -n $1 ] && [ "$1" != " " ] && [ "$1" != "" ]
+	then
+	echo $1
+	location=$1
+fi
+
 # There is an alias set up to call this file with, use it like so:
 
-# createrepositories /d/workspaces/helios_workspace/
+# createrepositories /d/workspaces/helios_workspace/ "Christopher McCulloh" cmcculloh@finishline.com
 
 # (replacing "/d/workspaces/helios_workspace/" with the path you want the workspaces created in)
 
 # You can run this script as many times in the same location as you want. If you already have one
 # or more of the repositories set up there (with the same naming scheme) it will just skip those
 
-cd $1
+cd $location
 
 git clone -o origin ssh://git@flgit.finishline.com/git/flgitscripts.git gitscripts
 git clone -o origin ssh://git@flgit.finishline.com/git/fl_deploy_scripts.git deploys
@@ -21,5 +32,17 @@ git clone -o origin ssh://git@flgit.finishline.com/git/finishline_csr.git finish
 git config --global color.status auto
 git config --global color.branch auto
 
-#git config --global user.name "Your Name Here"
-#git config --global user.email "Your Email Here"
+if [ -n "$2" ] && [ "$2" != " " ] && [ "$2" != "" ]
+	then
+	echo "$2"
+	git config --global user.name "$2"
+	
+	if [ -n "$3" ] && [ "$3" != " " ] && [ "$3" != "" ]
+		then
+		echo "$3"
+		git config --global user.email "$3"
+	fi
+fi
+
+
+cd $current_location
