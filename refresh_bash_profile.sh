dccb8325cd413e760d2944069fd56302b3fde83e
Chris Corwin <ccorwin@finishline.com>
2011-02-08 12:34:48 -0500

    (master) modified refresh _bash_profile.sh to look for a bash profile overrides file

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -3,7 +3,7 @@ echo "preparing to refresh your bash profile"
 source "${gitscripts_path}environment_config.default"
 source "${gitscripts_path}environment_config.overrides"
 
-cat "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" > "${gitscripts_path}bash_profile"
+cat "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" "${gitscripts_path}bash_profile_config.overrides" > "${gitscripts_path}bash_profile"
 
 cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
 cp "${gitscripts_path}motd" "${git_install}motd"
