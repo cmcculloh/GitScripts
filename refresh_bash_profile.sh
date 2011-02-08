4743df36b603ac59c1592cc829e209c08c740cd7
Christopher McCulloh <cmcculloh@finishline.com>
2011-02-08 14:19:34 -0500

    auto commit: added touch commands to create files if they don't exist. added line break between two of the files that get catted together

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -1,9 +1,12 @@
 echo "preparing to refresh your bash profile"
 
+touch "${gitscripts_path}bash_profile_config.overrides"
+touch "${gitscripts_path}environment_config.overrides"
+
 source "${gitscripts_path}environment_config.default"
 source "${gitscripts_path}environment_config.overrides"
 
-cat "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" "${gitscripts_path}bash_profile_config.overrides" > "${gitscripts_path}bash_profile"
+cat "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config.overrides" > "${gitscripts_path}bash_profile"
 
 cp "${gitscripts_path}bash_profile" "${git_install}bash_profile"
 cp "${gitscripts_path}motd" "${git_install}motd"
