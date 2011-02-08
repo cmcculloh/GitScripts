197542c9854fb0878dba471eca396a36817d8619
Christopher McCulloh <cmcculloh@finishline.com>
2011-02-08 10:48:46 -0500

    auto commit: trying to fix pushscripts

--- pullscripts.sh
+++ pullscripts.sh
@@ -5,15 +5,15 @@ cd ${gitscripts_path}
 
 current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
 
-echo before fetch
+echo "before fetch"
 git fetch --all --prune
-echo after fetch
+echo "after fetch"
 git checkout master
 git pull origin master
 git checkout $current_branch
 
-echo running refresh_bash_profile
+echo "running refresh_bash_profile"
 source refresh_bash_profile.sh
-echo ran refresh_bash_profile
+echo "ran refresh_bash_profile"
 
 cd $current_location
