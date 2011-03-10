daa15f581a47d08a4fddbc32089444d8f5a28c77
Christopher McCulloh <cmcculloh@finishline.com>
2011-03-10 10:49:50 -0500

    auto commit: made the checkout command more intelligent so that if you don't need to be prompted for anything it won't prompt you, it will just do it. However, if you have stuff that needs stashed etc it will ask you to do so

--- checkout.sh
+++ checkout.sh
@@ -4,97 +4,108 @@ echo "##########################################"
 echo
 echo
 
-echo git status
-git status
-echo
-echo
+branchexists=`git branch | grep "$1"`
 
-if [ "$1" = "dev" ] || [ $1 = "qa" ]
+if [ -n $branchexists ]
 	then
-	echo "delete your local copy and pull down new version to protect against forced updates? (y) n"
-	read deletelocal
-	if [ -z "$deletelocal" ] || [ "$decision" = "y" ]
+	if [ "$1" = "dev" ] || [ $1 = "qa" ]
 		then
-		trydelete=`git branch -d $1 2>&1 | grep "error"`
-		echo "$trydelete"
-		echo
-		if [ -n "$trydelete" ]
+		echo "delete your local copy of $1,"
+		echo "and pull down new version to protect against forced updates? (y) n"
+		read deletelocal
+		if [ -z "$deletelocal" ] || [ "$decision" = "y" ]
 			then
-			echo "Delete failed!"
-			echo "force delete? y (n)"
-			read forcedelete
-			if [ "$forcedelete" = "y" ]
+			trydelete=`git branch -d $1 2>&1 | grep "error"`
+			echo "$trydelete"
+			echo
+			if [ -n "$trydelete" ]
 				then
-				trydelete=`git branch -D $1 2>&1 | grep "error"`
-				echo "$trydelete"
-				echo
-				if [ -n "$trydelete" ]
+				echo "Delete failed!"
+				echo "force delete? y (n)"
+				read forcedelete
+				if [ "$forcedelete" = "y" ]
 					then
-					echo "force delete failed! continue anyways? y (n)"
+					trydelete=`git branch -D $1 2>&1 | grep "error"`
+					echo "$trydelete"
+					echo
+					if [ -n "$trydelete" ]
+						then
+						echo "force delete failed! continue anyways? y (n)"
+						read continueanyways
+						if [ -z "$continueanyways" ] || [ "$continueanyways" = "n" ]
+							then
+							exit -1
+						fi
+					else
+						echo "force delete succeeded!"
+					fi
+				else
+					echo "continue checking out $1? y (n)"
 					read continueanyways
 					if [ -z "$continueanyways" ] || [ "$continueanyways" = "n" ]
 						then
 						exit -1
 					fi
-				else
-					echo "force delete succeeded!"
 				fi
 			else
-				echo "continue checking out $1? y (n)"
-				read continueanyways
-				if [ -z "$continueanyways" ] || [ "$continueanyways" = "n" ]
-					then
-					exit -1
-				fi
+				echo "delete succeeded!"
+				echo
 			fi
 		else
-			echo "delete succeeded!"
+			echo "not deleteing your local copy of $1"
 			echo
 		fi
 	fi
 fi
 
+echo
+echo
+checkbranch=`git status | grep "nothing to commit (working directory clean)"`
+echo "$checkbranch"
 
-echo Type the number of the choice you want and hit enter
-echo "(1). Checkout $1"
-echo 2. Stash Changes and then checkout $1
-echo 3. Revert all changes to tracked files \(ignores untracked files\), and then checkout $1
-echo 4. Abort checkout of $1
-read decision
-
-
-if [ -z "$decision" ] || [ $decision -eq 1 ]
+if [ -z "$checkbranch" ]
 	then
-	echo continuing...
-elif [ $decision -eq 2 ]
-	then
-	echo This stashes any local changes you might have made and forgot to commit
-	echo git stash
-	git stash
 	echo
-	echo
-
-	echo git status
+	echo "git status"
 	git status
 	echo
 	echo
-elif [ $decision -eq 3 ]
-	then
-	echo This attempts to reset your current branch to the last checkin
-	echo if you have made changes to untracked files, this will not affect those
-	echo git reset --hard
-	git reset --hard
-	echo
-	echo
 
-	echo git status
-	git status
-	echo
-	echo
-else
-	exit 1
+	echo "You appear to have uncommited changes."
+	echo "(1). Stash Changes and then checkout $1"
+	echo "2. Revert all changes to tracked files \(ignores untracked files\), and then checkout $1"
+	echo "3. Abort checkout of $1"
+	read decision
+
+	if [ -z "$decision" ] || [ $decision -eq 1 ]
+		then
+		echo This stashes any local changes you might have made and forgot to commit
+		echo git stash
+		git stash
+		echo
+		echo
+
+		echo git status
+		git status
+		echo
+		echo
+	elif [ $decision -eq 2 ]
+		then
+		echo This attempts to reset your current branch to the last checkin
+		echo if you have made changes to untracked files, this will not affect those
+		echo git reset --hard
+		git reset --hard
+		echo
+		echo
+
+		echo git status
+		git status
+		echo
+		echo
+	else
+		exit 0
+	fi
 fi
-
 
 echo This tells your local git about all changes on fl remote
 echo git fetch --all --prune
