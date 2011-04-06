15ce4bdea4273eaba8d731dc074a5c866004bae1
Christopher McCulloh <cmcculloh@finishline.com>
2011-04-06 18:02:30 -0400

    auto commit: fixed syntax error in script

--- batch_automation/run-webapprefresh.bat
+++ batch_automation/run-webapprefresh.bat
@@ -1,3 +1,2 @@
-"C:\Program Files (x86)\Git\bin\bash.exe" --login -i -c "webapprefresh
- echo -e \"\a\""
+"C:\Program Files (x86)\Git\bin\bash.exe" --login -i -c "webapprefresh echo -e \"\a\""
 pause
