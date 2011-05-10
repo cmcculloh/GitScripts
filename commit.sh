aaf7afe00e7c7f1b53a16563bd17d5b6cae4336c
Chris Corwin <csc@9826-csc.(none)>
2011-05-10 12:27:47 -0400

    (making-ubuntu-gitscripts) added some new colors

--- commit.sh
+++ commit.sh
@@ -1,32 +1,105 @@
 #!/bin/bash
 
 
-echo
-echo git status
+
+
+echo ${H1}
+echo "##########################################"
+echo "Committing changes to branch ${COL_CYAN}$(cb)${COL_NORM}"
+echo "##########################################"
+echo ${X}
+echo ""
+
+
+echo ""
+echo ${O}
+echo "------------------------------------------"
+echo "# git status"
+#${TEXT_NORM}
+#${TEXT_BRIGHT}
 git status
-echo
+echo ${X}
 
 
-echo "git commit -q -m \"$(__git_ps1) $1\" $2"
+echo ""
+echo ${O}
+echo "------------------------------------------"
+echo "# git commit -q -m \"$(__git_ps1) $1\" $2"
 git commit -q -m "$(__git_ps1) $1" $2
+echo "------------------------------------------"
+echo ${X}
 
+
+echo ""
+echo ${O}
+echo "------------------------------------------"
+echo "# git diff-tree --stat HEAD"
 git diff-tree --stat HEAD
-
-echo
-
-
-git status
+echo "------------------------------------------"
+echo ${X}
 
 
 
 echo
 echo
 
+echo ${H2}
 echo "Would you like to push? y (n)"
+echo ${X}
 read YorN
 if [ "$YorN" = "y" ]
 	then
 	remote=$(git remote)
-	git push $remote head
+
+	echo ""
+	echo ${O}
+	echo "------------------------------------------"
+	echo "# git push $remote HEAD"
+	git push $remote HEAD
+	echo "------------------------------------------"
+	echo ${X}
+	echo
+
+
+	echo ${H2}
+	echo "##########################################"
+	echo "Status after push:"
+	echo "##########################################"
+	echo ${X}
+
+	echo ""
+	echo ${O}
+	echo "------------------------------------------"
+	echo "# git status"
+	#${TEXT_NORM}
+	#${TEXT_BRIGHT}
+	git status
+	echo ${X}
+
 fi
+
+
+echo "Check for remote changes? (y) n"
+echo ""
+read YorN
+if [ "$YorN" = "y" ] || [ "$YorN" = "" ]
+	then
+	
+	echo "NOT ACTUALLY CHECKING FOR CHANGES RIGHT NOW --- TBD LATER"
+fi
+
+
+
+echo "Clear screen? (y) n"
+read YorN
+if [ "$YorN" = "y" ] || [ "$YorN" = "" ]
+	then
+
+	echo ""
+	echo "Clearing screen now!"
+	echo ""
+	clear
+fi
+
+
 
