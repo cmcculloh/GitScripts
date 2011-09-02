0f73e8d57e1302207e8408f90ac8db1015b60b57
Christopher McCulloh <cmcculloh@finishline.com>
2011-09-02 15:27:00 -0400

    (master) made branch command accept a partial branch name that it would try to match and then ask if you wanted to check out the first match

---
+++ grepbranch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+git branch -a | grep "$1"
+res=$(git branch -a | grep "$1" | head -1 | sed 's/fl\///')
+
+echo""
+echo "  Checkout ${res}? y (n)"
+read YorN
+if [ "$YorN" = "y" ]
+	then
+	${gitscripts_path}checkout.sh ${res}
+fi
