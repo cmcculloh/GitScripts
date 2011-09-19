2ca88e2535b4e2d395145e3770b4e6f3b1ac9774
Christopher McCulloh <cmcculloh@finishline.com>
2011-09-19 14:16:49 -0400

    (master) made filter out remotes/ as well as fl/

--- grepbranch.sh
+++ grepbranch.sh
@@ -2,7 +2,7 @@
 
 
 git branch -a | grep "$1"
-res=$(git branch -a | grep "$1" | head -1 | sed 's/fl\///')
+res=$(git branch -a | grep "$1" | head -1 | sed 's/fl\///' | sed 's/remotes\///')
 
 echo""
 echo "  Checkout ${res}? y (n)"
