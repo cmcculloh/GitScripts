7f9e1cfb23c702d897655df1ec3c701cbab0d8a4
Chris Corwin <ccorwin@finishline.com>
2011-12-23 12:54:17 -0500

    (corwin-changes-with-gitscripts-submodule) WIP making refresh_bash_profile not overwrite .bashrc

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -8,27 +8,28 @@ fi
 pushd . > /dev/null
 cd `dirname ${SCRIPT_PATH}` > /dev/null
 export SCRIPT_PATH=`pwd`;
-export flscripts_path="${SCRIPT_PATH}/"
+export flgitscripts_path="${SCRIPT_PATH}/"
 popd  > /dev/null
 
 
 echo "Preparing to refresh your bash profile..."
 
-touch "${flscripts_path}bash_profile_config.overrides"
-touch "${flscripts_path}environment_config.overrides"
+touch "${flgitscripts_path}bash_profile_config.overrides"
+touch "${flgitscripts_path}environment_config.overrides"
 
-source "${flscripts_path}environment_config.overrides"
-source "${flscripts_path}environment_config.default"
+source "${flgitscripts_path}environment_config.overrides"
+source "${flgitscripts_path}environment_config.default"
 
-touch "${flscripts_temp_bash_profile_path}"
+touch "${flgitscripts_temp_bash_profile_path}"
 
 echo "	> concatenating scripts..."
-cat "${flscripts_path}environment_config.overrides" "${flscripts_path}line_break" "${flscripts_path}environment_config.default" "${flscripts_path}line_break" "${flscripts_path}bash_profile_config.overrides" "${flscripts_path}line_break" "${flscripts_path}bash_profile_config" > "${flscripts_temp_bash_profile_path}"
+cat "${flgitscripts_path}environment_config.overrides" "${flgitscripts_path}line_break" "${flgitscripts_path}environment_config.default" "${flgitscripts_path}line_break" "${flgitscripts_path}bash_profile_config.overrides" "${flgitscripts_path}line_break" "${flgitscripts_path}bash_profile_config" > "${flgitscripts_temp_bash_profile_path}"
 
 
 echo "	> sending scripts to etc..."
-cp "${flscripts_temp_bash_profile_path}" "${native_bash_profile_path}"
-#rm "${flscripts_temp_bash_profile_path}"
+cp -p -f "${gitscripts_temp_bash_profile_path}" "${native_gitscripts_bash_profile_path}"
+
+#rm "${flgitscripts_temp_bash_profile_path}"
 
 ${STYLE_NORM}
 
