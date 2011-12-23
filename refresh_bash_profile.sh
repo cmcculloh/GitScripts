f7d37a816ac740d38869f1c9b56393afd24fd735
Chris Smola <csmola@finishline.com>
2011-12-23 10:44:21 -0500

    up to date from last night after forgotten push

--- refresh_bash_profile.sh
+++ refresh_bash_profile.sh
@@ -1,51 +1,38 @@
+# need to know current path of this file first
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if [ -h "${SCRIPT_PATH}" ]; then
+	while [ -h "${SCRIPT_PATH}" ]; do
+		SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`
+	done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+export SCRIPT_PATH=`pwd`;
+export flscripts_path="${SCRIPT_PATH}/"
+popd  > /dev/null
+
+
 echo "Preparing to refresh your bash profile..."
 
-touch "${gitscripts_path}bash_profile_config.overrides"
-touch "${gitscripts_path}environment_config.overrides"
+touch "${flscripts_path}bash_profile_config.overrides"
+touch "${flscripts_path}environment_config.overrides"
 
-source "${gitscripts_path}environment_config.overrides"
-source "${gitscripts_path}environment_config.default"
+source "${flscripts_path}environment_config.overrides"
+source "${flscripts_path}environment_config.default"
 
+touch "${flscripts_temp_bash_profile_path}"
 
 echo "	> concatenating scripts..."
-cat "${gitscripts_path}environment_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}environment_config.default" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config.overrides" "${gitscripts_path}line_break" "${gitscripts_path}bash_profile_config" > "${gitscripts_temp_bash_profile_path}"
+cat "${flscripts_path}environment_config.overrides" "${flscripts_path}line_break" "${flscripts_path}environment_config.default" "${flscripts_path}line_break" "${flscripts_path}bash_profile_config.overrides" "${flscripts_path}line_break" "${flscripts_path}bash_profile_config" > "${flscripts_temp_bash_profile_path}"
 
 
 echo "	> sending scripts to etc..."
-cp "${gitscripts_temp_bash_profile_path}" "${native_bash_profile_path}"
-cp "${gitscripts_motd_path}" "${native_motd_path}"
-
-rm "${gitscripts_temp_bash_profile_path}"
-
-
-echo "	> sourcing: set-bash-colors.sh && bashmarks.sh"
-source ${gitscripts_path}set-bash-colors.sh
-source ${gitscripts_path}bashmarks.sh
+cp "${flscripts_temp_bash_profile_path}" "${native_bash_profile_path}"
+#rm "${flscripts_temp_bash_profile_path}"
 
 
 echo "	> sourcing: ${native_bash_profile_path}"
 source "${native_bash_profile_path}"
-
-
-## ADDED BY SMOLA ##
-
-	#prevent saving of *.orig files during git diff
-git config --global mergetool.keepBackup false
-
-	#some vars
-export workspacedir="/d/workspaces/helios_workspace"
-export myscriptsdir="${workspacedir}/myscripts"
-
-	#added scripts
-unalias branch; alias branch="${gitscripts_path}grepbranch.sh"
-alias finddocs="${myscriptsdir}/finddocrefs.sh"
-alias getdirs="${myscriptsdir}/getdirs.sh"
-alias promosetup="${myscriptsdir}/promosetup.sh"
-
-alias mastermerges="${myscriptsdir}/checkoutbranches.sh"
-alias cleanbranches="${myscriptsdir}/cleanbranches.sh"
-alias switchenv="${myscriptsdir}/switchenvironment.sh"
-alias whichenv="${myscriptsdir}/whichenvironment.sh"
 
 echo
 echo ${TEXT_BRIGHT}"Your bash profile has been refreshed!"${X}
