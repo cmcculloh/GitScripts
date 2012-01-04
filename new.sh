381d47f744b467a25cb531176d8ca3d0a9773056
Chris Corwin <ccorwin@finishline.com>
2012-01-04 15:34:14 -0500

    (make-new-dot-sh-have-prettier-messaging) new colors, applied to new.sh

--- new.sh
+++ new.sh
@@ -1,29 +1,42 @@
-branch="master"
+$loadfuncs
+
+startingBranch="master"
 if [ -n $3 ] && [ "$3" != " " ] && [ "$3" != "" ]
 	then
-	branch=$3
+	startingBranch=$3
 fi
 
-echo "##########################################"
-echo Creating new branch $1 from $branch
-echo "##########################################"
-echo
-echo
-echo git status
-git status
-echo
-echo
 
-echo Type the number of the choice you want and hit enter
-echo "(1). Create branch $1 from $branch"
-echo 2. Stash Changes and create branch $1 from $branch
-echo 3. Revert all changes to tracked files \(ignores untracked files\), and create branch $1 from $branch
-echo 4. Abort creation of branch $1 from $branch
+echo ${H1}
+echo ${H1HL}
+echo "Creating new branch: ${STYLE_NEWBRANCH_H1}\`${1}\`${H1} based off of ${STYLE_OLDBRANCH_H1}\`${startingBranch}\`${H1} "
+echo ${H1HL}
+echo ${X}
+echo ${O}
+echo ${H2HL}
+echo "# git status --porcelain"
+git status --porcelain
+echo ${X}${I}
+echo ${HL}
+echo "Type the number of the choice you want and hit enter"
+echo " (1) -  Create branch ${STYLE_NEWBRANCH}\`${1}\`${I} from ${STYLE_OLDBRANCH_H1}\`${startingBranch}\`${I}"
+echo "  2  -  Stash Changes and create branch $1 from ${STYLE_OLDBRANCH_H1}\`${startingBranch}\`${I}"
+echo "  3  -  Revert all changes to tracked files \(ignores untracked files\), and create branch $1 from ${STYLE_OLDBRANCH_H1}\`${startingBranch}\`${I}"
+echo "  4  -  Abort creation of branch $1 from ${STYLE_OLDBRANCH_H1}\`${startingBranch}\`${I}"
+echo ${HL}${X}
 read decision
-echo You chose: $decision
+echo 
+echo "${O}You chose: $decision"
+echo ${X}
+
 if [ -z "$decision" ] || [ $decision -eq 1 ]
 	then
 	echo continuing...
+elif [ $decision -eq 4 ]
+	then
+	echo "Aborting creation of branch ${STYLE_NEWBRANCH}\`${1}\`${X}"
+	exit 1
+	
 elif [ $decision -eq 2 ]
 	then
 	echo This stashes any local changes you might have made and forgot to commit
@@ -50,6 +63,7 @@ elif [ $decision -eq 3 ]
 	echo
 	echo
 else
+
 	exit 1
 fi
 
@@ -61,7 +75,7 @@ git fetch --all --prune
 echo
 echo
 
-if [ "$branch" = "master" ]
+if [ "$startingBranch" = "master" ]
 	then
 	echo
 	echo
@@ -99,21 +113,21 @@ if [ "$branch" = "master" ]
 else
 	echo
 	echo
-	checkout.sh $branch
+	checkout.sh $startingBranch
 
-	echo "You are about to branch $branch to create a new branch named $1"
-	echo 'YOU SHOULD PROBABLY NOT BE DOING THIS!!!!'
-	echo "The only reason to do this is if your new branch relies on branch $branch"
-	echo 'Please enter "I understand" and hit enter to continue'
+	echo "You are about to checkout branch $startingBranch in order to create a new branch named $1"
+	echo 'Do not do this unless you truly know what you are doing, and why!'
+	echo "The only reason to do this is if your new branch relies on branch $startingBranch"
+	echo 'Please type "I understand" and hit enter to continue'
 	echo 'or type anything else or just hit enter to abort'
 	read iunderstand
 	if [ "$iunderstand" = "I understand" ]
 		then
-		echo This branches $branch to create a new branch named $1
-		echo git checkout -b $1 $branch
-		git checkout -b $1 $branch
-		git config branch.$branch.remote $remote
-		git config branch.$branch.merge refs/heads/$branch
+		echo This branches $startingBranch to create a new branch named $1
+		echo git checkout -b $1 $startingBranch
+		git checkout -b $1 $startingBranch
+		git config branch.$1.remote $remote
+		git config branch.$1.merge refs/heads/$1
 		git push $remote $1
 
 	else
