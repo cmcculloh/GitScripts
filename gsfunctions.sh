69975f70dbbfffbbec47aa79190b4e0924e5e882
Chris Smola <csmola@finishline.com>
2012-01-05 11:44:34 -0500

    (csmola---colorize-new) Colorized the new script, added function to parse a remote, documented new.

--- gsfunctions.sh
+++ gsfunctions.sh
@@ -154,6 +154,71 @@ function __branch_exists {
 
 
 ## /* @function
+#	@usage __get_remote
+#
+#	@output true
+#
+#	@description
+#	This function searches all the remotes for a git project and will present a menu
+#	to choose one if multiple remotes have been configured. If only one remote has been configured
+#	it will bypass the menu process and simply return that remote string. If no remotes are
+#	configured, there will be no output.
+#	description@
+#
+#	@notes
+#	- This function is intended to be used for it's output, not in conditionals.
+#	notes@
+#
+#	@examples
+#	...
+#	remote=$(__get_remote)
+#	git push $remote branch-name-to-push
+#	...
+#	examples@
+## */
+function __get_remote {
+	remotes_string=$(git remote);
+
+	# if no remotes are configured there's no reason to continue processing.
+	if [ -z "$remotes_string" ]; then
+		exit 1
+	fi
+
+	c=0;
+	for remote in $remotes_string; do
+		remotes[$c]=$remote;
+		(( c++ ));
+	done
+
+	# if more than one remote exists, give the user a choice.
+	if [ ${#remotes[@]} -gt 1 ]; then
+		echo ${O}${H2HL}
+		for (( i = 0 ; i < ${#remotes[@]} ; i++ )); do
+			remote=$(echo ${remotes[$i]} | sed 's/[a-zA-Z0-9\-]+(\/\{1\}[a-zA-Z0-9\-]+)//p')
+
+			if [ $i -le "9" ]; then
+				index="  "$i
+			elif [ $i -le "99" ]; then
+				index=" "$i
+			else
+				index=$i
+			fi
+			echo "$index: $remote"
+		done
+		echo ${H2HL}${X}
+		echo ${I}"Choose a remote (or just hit enter to abort):"
+		read remote
+		echo ${X}
+
+		remote=$(echo ${remotes[$remote]} | sed 's/\// /')
+	else
+		remote=${remotes[0]}
+	fi
+	echo "$remote"
+}
+
+
+## /* @function
 #	@usage __parse_git_branch
 #
 #	@output true
