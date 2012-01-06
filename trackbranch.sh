92dbaa50e09e771f889b8f6f3c407e41307269c2
Chris Corwin <ccorwin@finishline.com>
2012-01-06 10:18:36 -0500

    (csc---make-command-to-set-up-remote-tracking-on-existing-branch) WIP, essentially a stash. new trackbranch script started. a couple of colors added

---
+++ trackbranch.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+## /*
+#	@usage trackbranch [<branch_name>] 
+#
+#	@description
+#	This script allows an easy way to set the upstream for an existing branch. 
+#	
+#	If there is a remote branch of the same name in existance, it will choose it.
+#	
+#	If it does not find a remote branch it will push the <branch_name> to the remote.
+#	
+#	It will then set the upstream for the local branch to the remote branch of the same name.
+#	description@
+#
+#	@notes
+#	- It uses the git command git branch --set-upstream foo upstream/foo
+#	notes@
+#
+#	@examples
+#	1) trackbranch
+#	2) trackbranch foo
+#	examples@
+#
+#	@dependencies
+#	gitscripts/gsfunctions.sh
+#	dependencies@
+## */
+$loadfuncs
+
+
+if [ -z "$1" ]; then
+	# no parameter supplied, we shall determine current branch
+	startingBranch=$(__parse_git_branch)
+	if [ -z "$startingBranch" ]; then
+		echo ${E}"Error: a failure has occured."${X}
+		echo ${E}"Unable to determine current branch, and a branch name was not supplied. Exiting now."${X}
+		exit 1
+	fi
+
+	echo "No branch name supplied, we have determined that it is: ${startingBranch}"
+
+elif __branch_exists $1; then
+	# parameter supplied --- and branch exists
+	echo "Branch exists!"
+else
+	# parameter supplied --- and branch does not exist
+	echo ${E}"Error: a failure has occured."${X}
+	echo ${E}"The branch supplied (${1}) does not appear to exist. Exiting now."${X}
+	exit 1
+
+fi
+
+
+
+# git config --get branch.master.merge
+
