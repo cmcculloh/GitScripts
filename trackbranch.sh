61dfb7e82273c941d1e4e9f9c5f08cdec6bd02cb
Chris Corwin <ccorwin@finishline.com>
2012-01-06 11:10:12 -0500

    (csc---make-command-to-set-up-remote-tracking-on-existing-branch) WIP

--- trackbranch.sh
+++ trackbranch.sh
@@ -29,6 +29,7 @@
 $loadfuncs
 
 
+
 if [ -z "$1" ]; then
 	# no parameter supplied, we shall determine current branch
 	startingBranch=$(__parse_git_branch)
@@ -38,30 +39,70 @@ if [ -z "$1" ]; then
 		exit 1
 	fi
 
-	echo "No branch name supplied, we have determined that it is: ${startingBranch}"
+	#echo "No branch name supplied, we have determined that it is: ${startingBranch}"
 
-	echo "Will check to see if it is being tracked already..."
-	if __branch_merge_set $startingBranch; then
-		echo "It is already tracked."
-		exit 1
-	else
-		echo "It is NOT already tracked."
-	fi
 
 elif __branch_exists $1; then
 	# parameter supplied --- and branch exists
-	echo "Branch exists!"
+	#echo "Branch exists!"
 	startingBranch=$1
 else
 	# parameter supplied --- and branch does not exist
 	echo ${E}"Error: a failure has occured."${X}
-	echo ${E}"The branch supplied (${1}) does not appear to exist. Exiting now."${X}
+	echo ${E}"The branch supplied (${COL_CYAN}${1}${COL_NORM}) does not appear to exist. Exiting now."${X}
 	exit 1
 
 fi
 
 
 
-# git config --get branch.master.merge
+echo ${H1}
+echo ${H1HL}
+echo "Going to set up a remote tracking branch for: ${COL_CYAN}${startingBranch}${COL_NORM}"
+echo ${H1HL}
+echo ${X}
+echo
+
+
+
+if __branch_merge_set $startingBranch; then
+	echo ${STYLE_WARNING}"Alert: the branch ${COL_CYAN}${startingBranch}${COL_NORM}${STYLE_WARNING} is already tracking a remote branch. Exiting now."${X}
+	exit 1
+else
+	echo ${STYLE_NOTIFY}"The branch ${COL_CYAN}${startingBranch}${COL_NORM}${STYLE_WARNING} is not yet tracking a remote branch. Determining remote now..."${X}
+	remote=$(__get_remote)
+
+	if [ -n "$remote" ]; then
+		echo "Remote: ${COL_GREEN}${remote}${COL_NORM}"
+		echo
+		echo "$ git fetch --all --prune"
+		git pull fetch --all --prune
+		echo
+		echo
+		# if a remote exists, push to it.
+		echo
+		echo
+		echo ${STYLE_NOTIFY}"Remote determined to be ${COL_CYAN}${remote}${COL_NORM}${STYLE_WARNING}. Pushing to remote now..."${X}
+		echo ${O}${H2HL}
+		echo "$ git push ${remote} ${startingBranch}"
+		git push $remote $startingBranch
+		echo ${H2HL}${X}
+	fi
+
+	echo ${H2HL}${X}
+	git config branch.$startingBranch.remote $remote
+	git config branch.$startingBranch.merge refs/heads/$startingBranch
+
+	echo "But it should be now..."
+	if __branch_merge_set $startingBranch; then
+		echo "Yes, it does!"
+		exit 0
+	else
+		echo "Uh oh."
+		exit 1
+	fi
+	
+fi
+
 # git config --get branch.csc---make-command-to-set-up-remote-tracking-on-existing-branch.merge
 
