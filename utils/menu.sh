1d61e9241ef7eebf103fead27f6f57aa02f68db7
Christopher McCulloh <cmcculloh@finishline.com>
2012-01-09 17:54:15 -0500

    (menu) got the array passing to work (finally, OMG this was ridiculous)

--- utils/menu.sh
+++ utils/menu.sh
@@ -6,6 +6,24 @@
 #	@description
 #	Takes an array and outputs each element as a selectable item
 ## */
-function __menu {
-	echo "here"
+__menu() {
+	local items=$1[@]
+	items=(${!items})
+	for (( i = 0 ; i < ${#items[@]} ; i++ ))
+		do
+		#TODO: make it so that you can pass through a sed value (or two or three) to be run on each item
+		item=$(echo ${items[$i]})
+
+		if [ $i -le "9" ] ; then
+			index="  "$i
+		elif [ $i -le "99" ] ; then
+			index=" "$i
+		else
+			index=$i
+		fi
+		echo "$index: $item"
+	done
 }
+
+arr=(bolah lah blah)
+__menu arr
