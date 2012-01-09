3a1d727199d0ee8dade179e7ec3375f917bc2193
Chris Corwin <ccorwin@finishline.com>
2012-01-09 13:00:11 -0500

    (clean-branches-should-list-branch-name-specifically) formatted clean-branches list with colors and commented it

--- clean-branches.sh
+++ clean-branches.sh
@@ -1,6 +1,35 @@
-current_branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+#!/bin/bash
 
-${gitscripts_path}checkout.sh master
+## /*
+#	@usage clean-branches
+#
+#	@description
+#	This script iterates through your local branches and prompts you to delete branche that are fully merged into master.
+#	description@
+#
+#	@notes
+#	- You will end on the same branch you started out on.
+#	notes@
+#
+#	@examples
+#	1) clean-branches
+#	examples@
+#
+#	@dependencies
+#	gitscripts/gsfunctions.sh
+#	dependencies@
+## */
+$loadfuncs
+
+startingBranch=$(__parse_git_branch)
+if [ -z "$startingBranch" ]; then
+	echo ${E}"Unable to determine current branch."${X}
+	exit 1
+fi
+
+
+
+# ${gitscripts_path}checkout.sh master
 
 for branch in `git branch`
 do
@@ -12,12 +41,46 @@ do
 			then
 			if [ "$branch" != "master" ]
 				then
+
+				echo ${H2}${H2HL}
+				echo "${STYLE_NEWBRANCH_H2}\`${branch}\`${H2}  appears to be merged into the following branches:"
+				echo ${H2HL}${X}
 				echo
-				echo "git branch --contains \"${branch}\""
-				echo "indicates the following branches appear to have this branch merged into them:"
-				git branch --contains "${branch}"
-				echo
+				# git branch --contains "${branch}"
+
+				# echo "git branch --contains \"${branch}\""
+
+				# branchesContainingThisOne=`git branch --no-color --contains "${branch}"` | awk '{gsub(/\* /, "");print;}'
+				branchesContainingThisOne=`git branch --no-color --contains "${branch}" | awk '{gsub(/\* /, "");print;}'`
+				# branchesContainingThisOne=`git branch --no-color --contains "${branch}"` | awk '{gsub(/\* /, "");print;}'
+
+
+
+				echo ${O}${H2HL}
+
+				for aBranch in $branchesContainingThisOne
+				do
+
+					# echo "aBranch: \"${aBranch}\""
+					# echo "git branch | grep \"${aBranch}\""
+					wellformed=`git branch | grep "${aBranch}"`
+					# echo "wellformed: ${wellformed}"
+
+					if [ -n "$wellformed" ]
+						then
+						branchcontains=`git branch --contains "${branch}" | grep "${aBranch}"`
+						if [ -n "$branchcontains" ]
+							then
+							echo "${aBranch}"
+							# echo "${COL_RED}$aBranch${COL_NORM}"
+						fi
+					fi
+				done
+				echo ${H2HL}
+
+				echo ${STYLE_INPUT}
 				echo "  delete $branch? (y) n"
+				echo ${X}
 				read decision
 				if [ -z $decision ] || [ "$decision" = "y" ]
 					then
@@ -29,4 +92,4 @@ do
 	fi
 done
 
-git checkout $current_branch
+git checkout $startingBranch
