26c89356d994d2f90bb7ee029bee1fbac4aab522
Chris Corwin <ccorwin@finishline.com>
2012-01-11 13:41:18 -0500

    (master) updated shebangs

--- createRepositories.sh
+++ createRepositories.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 current_location=$(pwd)
 
--- git-diff-wrapper.sh
+++ git-diff-wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # diff is called by git with 7 parameters:
 #  path old-file old-hex old-mode new-file new-hex new-mode
