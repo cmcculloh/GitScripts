f871cbe22ab9410207e5df14afcd2cdc0e9b1988
Chris Smola <csmola@finishline.com>
2012-01-13 16:10:23 -0500

    (csmola---fix-branch-command-menu)

--- branch.sh
+++ branch.sh
@@ -1,62 +1,82 @@
 #!/bin/bash
+## /*
+#	@usage branch [options] [search-string]
+#
+#	@description
+#	This is a handy tool to filter local and/or remote branch names in your repository.
+#	It shows all branches as a menu and allows you to choose the branch by index.
+#	Default behavior is to show BOTH local and remote branches. To filter further, use
+#	one of the options below
+#	description@
+#
+#	@options
+#	-l, --local     Show only local branches.
+#	-r, --remote    Show only remote branches.
+#	options@
+#
+#	@notes
+#	- Search string CANNOT begin with a hyphen!
+#	- Passing both options above will result in showing ALL branches as expected.
+#	notes@
+#
+#	@examples
+#	1) branch --local part-of-bran
+#	   # filters local branches that match "part-of-bran"
+#	2) branch -r
+#	   # shows ALL remote branches
+#	examples@
+#
+#	@dependencies
+#	functions/0300.menu.sh
+#	dependencies@
+## */
+$loadfuncs
 
+echo ${X}
 
-git branch -a | grep "$1"
-remotes_string=$(git branch -a | grep "$1" | sed 's/fl\///' | sed 's/remotes\///');
-c=0;
-
-last=""
-for remote in $remotes_string; 
-do 
-#echo "$c: $remote";
-r=$(echo $remote)
-if [ "$last" != "$r" ]; then
-	remotes[$c]=$r;
-	c=$((c+1));
-fi
-
-last=$r
-done
-
-if [ ${#remotes[@]} -gt 1 ]
-	then
-	echo ${O}"------------------------------------------------------------------------------------"
-	echo "Choose a branch (or just hit enter to abort):"
-	echo "------------------------------------------------------------------------------------"
-	for (( i = 0 ; i < ${#remotes[@]} ; i++ ))
-		do
-		remote=$(echo ${remotes[$i]} | sed 's/[a-zA-Z0-9\-]+(\/\{1\}[a-zA-Z0-9\-]+)//p')
-
-		if [ $i -le "9" ] ; then
-			index="  "$i
-		elif [ $i -le "99" ] ; then
-			index=" "$i
-		else
-			index=$i
-		fi
-		echo "$index: $remote"
+# parse params
+numArgs=$#
+if (( numArgs > 0 && numArgs < 3 )); then
+	until [ -z "$1" ]; do
+		{ [ "$1" = "-l" ] || [ "$1" = "--local" ]; } && getLocal=true
+		{ [ "$1" = "-r" ] || [ "$1" = "--remote" ]; } && getRemote=true
+		! echo "$1" | egrep -q "^-" && search="$1"
+		shift
 	done
-	echo ${I}"Choose a branch (or just hit enter to abort):"
-	read remote
-	echo ${X}
 
-	if [ -n "$remote" ]; then
-		remote=$(echo ${remotes[$remote]} | sed 's/[a-z]*\///g')
-	else
-		echo "no branch chosen, aborting"
-		exit 0
+	flag="-a"
+	if [ $getLocal ] && [ ! $getRemote ]; then
+		flag=""
+	elif [ $getRemote ] && [ ! $getLocal ]; then
+		flag="-r"
 	fi
 fi
 
+# grab and parse meta from branches
+branches=$(git branch $flag | grep "$search" | sed 's/*/ /' | sed 's/remotes\///' | awk '{ if ($0 !~ /.+ -> .+/) print; }');
 
-if [ -n "$remote" ]; then
-	echo""
-	echo "  Checkout ${remote}? y (n)"
-	read YorN
-	if [ "$YorN" = "y" ]
-		then
-		${gitscripts_path}checkout.sh ${remote}
+# generate menu
+[ -n "$branches" ] && __menu "$branches" || {
+	echo ${O}"No branches found!"${X}
+	exit 1
+}
+
+# process selection
+if [ $_menu_selection ]; then
+	# gather remotes to remove the name from the branch before checking it out
+	remotes=$(git remote)
+	branch="$_menu_selection"
+	for remote in $remotes; do
+		branch=$(echo "$branch" | sed "s/${remote}\///")
+	done
+
+	echo
+	echo ${Q}"  Checkout ${B}\`${branch}\`${Q}? y (n)  "${X}
+	read yn
+	if [ "$yn" = "y" ] || [ "$yn" = "Y" ]; then
+		echo
+		"${gitscripts_path}"checkout.sh "$branch"
 	fi
-else
-	echo "no known branches on any remotes match or contain that name"
 fi
+
+exit
