cb0e2c09f328d81e0b8ba11cc5ca31aadda4e402
Chris Smola <csmola@finishline.com>
2012-01-19 19:29:04 -0500

    (csmola---change-__menu-option-parsing) WIP...new variables set in function, option parsing changed. need to alter calling scripts.

--- functions/0300.menu.sh
+++ functions/0300.menu.sh
@@ -4,7 +4,8 @@
 #	@output true
 #
 #	@vars
-#	$_menu_selection
+#	$_menu_sel_index
+#	$_menu_sel_value
 #	vars@
 #
 #	@description
@@ -51,13 +52,12 @@
 ## */
 
 __menu() {
-	local extraList
+	declare -a extraItems
 	local i
 	local index
 	local item
-	local items
+	declare -a items
 	local j
-	local list
 	local opt
 	local pair
 	local prompt
@@ -66,20 +66,21 @@ __menu() {
 	if (( numArgs > 0 && numArgs < 4 )); then
 		until [ -z "$1" ]; do
 			echo "$1" | egrep -q "^--prompt=" && prompt=$( echo "$1" | awk '{ print substr($0,10); }' )
-			echo "$1" | egrep -q "^:" && extraList="$1"
-			! echo "$1" | egrep -q "^(--prompt|:)" && list="$1"
+			echo "$1" | egrep -q "^:" && extraItems[${#extraItems[@]}]="$1"
+			! echo "$1" | egrep -q "^(--prompt|:)" && items[${#items[@]}]="$1"
 			shift
 		done
 	fi
 
-	if [ -z "$list" ]; then
-		__gslog "__menu: First parameter must be white-space separated list. Given: $@"
-		echo ${E}"  __menu did not detect a list to display. Aborting...  "${X}
+	if [ ${#items[@]} -eq 0 ] && [ ${#extraItems[@]} -eq 0 ]; then
+		__gslog "__menu: No lists given. Given: $@"
+		echo ${E}"  __menu did not detect any list items to display. Aborting...  "${X}
 		return 1
 	fi
 
-	# reset output variable
-	_menu_selection=""
+	# reset output variables
+	export _menu_sel_index=
+	export _menu_sel_value=
 
 	# check for custom message
 	local msg="Please make a selection (or press Enter to abort)"
@@ -88,37 +89,39 @@ __menu() {
 	fi
 
 	# build menu
-	items=( $list )
 	echo ${STYLE_MENU_HL}${H2HL}${X}
 	echo ${STYLE_MENU_HEADER}"  $msg  "${X}
 	echo ${STYLE_MENU_HL}${H2HL}${X}
-	for (( i = 1 ; i <= ${#items[@]} ; i++ )); do
-		j=$(( i - 1 ))
-		item="${items[$j]}"
 
-		# make indexes right-aligned. works for up to 999 choices.
-		if (( i < 10 )); then
-			index="  "$i
-		elif (( i < 100 )); then
-			index=" "$i
-		else
-			index=$i
-		fi
-		echo "  ${STYLE_MENU_INDEX}${index}:${X}  ${STYLE_MENU_OPTION}${item}${X}"
-	done
-	echo ${STYLE_MENU_HL}${H2HL}${X}
+	if [ ${#items[@]} -gt 0 ]; then
+		for (( i = 1 ; i <= ${#items[@]} ; i++ )); do
+			j=$(( i - 1 ))
+			item="${items[$j]}"
+
+			# make indexes right-aligned. works for up to 999 choices.
+			if (( i < 10 )); then
+				index="  $i"
+			elif (( i < 100 )); then
+				index=" $i"
+			else
+				index="$i"
+			fi
+			echo "  ${STYLE_MENU_INDEX}${index}:${X}  ${STYLE_MENU_OPTION}${item}${X}"
+		done
+		echo ${STYLE_MENU_HL}${H2HL}${X}
+	fi
 
 	# If extra list is given, parse
-	if [ -n "$extraList" ]; then
-		declare -a extraItems
+	if [ ${#extraItems[@]} -gt 0 ]; then
+		declare -a parsedItems
 		declare -a ndxes
 		declare -a vals
 		i=0
-		for pair in $extraList; do
-			extraItems[$i]=$(echo "$pair" | awk -f "${gitscripts_awk_path}menu_extra_option_parse.awk")
+		for pair in ${extraItems[@]}; do
+			parsedItems[$i]=$(echo "$pair" | awk -f "${gitscripts_awk_path}menu_extra_option_parse.awk")
 			# echo "${extraItems[$i]}"
-			ndxes[$i]=$(echo "${extraItems[$i]}" | cut -f 1 -d" ")
-			vals[$i]=$(echo "${extraItems[$i]}" | cut -f 2- -d" ")
+			ndxes[$i]=$(echo "${parsedItems[$i]}" | cut -f 1 -d" ")
+			vals[$i]=$(echo "${parsedItems[$i]}" | cut -f 2- -d" ")
 
 			# printf was not working when providing the field width specifier. Scripts that call this
 			# function are most likely getting called as subprocesses, which have some leading/trailing
@@ -135,30 +138,30 @@ __menu() {
 	read opt
 
 	# validate response
-	if [ -z "$opt" ]; then
-		return 0
+	if [ -n "$opt" ]; then
+		_menu_sel_index="$opt"
+		if [ ${#extraItems[@]} -gt 0 ] && __in_array "$opt" "${ndxes[@]}"; then
+			_menu_sel_value="${vals[${_in_array_index}]}"
 
-	elif [ -n "$extraList" ] && __in_array "$opt" "${ndxes[@]}"; then
-		_menu_selection="${vals[${_in_array_index}]}"
+		elif echo "$opt" | egrep -q '^[[:digit:]]+$'; then
+			local optndx
+			(( optndx = opt - 1 ))
+			if [ -n "${items[$optndx]}" ]; then
+				_menu_sel_value="${items[$optndx]}"
+			fi
 
-	elif echo "$opt" | egrep -q '^[[:digit:]]+$'; then
-		(( optndx = opt - 1 ))
-		if [ -n "${items[$optndx]}" ]; then
-			_menu_selection="${items[$optndx]}"
+		else
+			echo
+			echo ${E}"  Invalid selection! Aborting...  "${X}
+			return 1
 		fi
-	fi
-
-	# if the export variable hasn't been set, there was a problem
-	if [ -z "$_menu_selection" ]; then
-		echo
-		echo ${E}"  Invalid selection! Aborting...  "${X}
-		return 1
 	fi
 
 	#wrap up...
 	echo
-	echo ${O}"  You chose:${X} ${_menu_selection}  "
-	export _menu_selection
+	echo ${X}"  You chose: ${_menu_sel_index}"
+	export _menu_sel_index
+	export _menu_sel_value
 
 	return 0
 }
