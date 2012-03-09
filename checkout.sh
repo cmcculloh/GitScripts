1261e02c55dcd107194ba3b99b9b7c93cf7958dc
Christopher McCulloh <cmcculloh@finishline.com>
2012-03-09 13:11:19 -0500

    (master) made checkout call branch if full branch name not passed to checkout

--- checkout.sh
+++ checkout.sh
@@ -60,9 +60,10 @@ fi
 patt="[^/]*\/"
 branch=${1/$patt/}
 
-
 # If the user made it this far, they have passed the branch name as the first parameter to
 # the script. Additional processing occurs. Begin by establishing where the branch exists.
+onremote=
+onlocal=
 __branch_exists_remote "$branch" && onremote=true
 __branch_exists_local "$branch" && onlocal=true
 
@@ -71,10 +72,10 @@ cb=$(__parse_git_branch)
 if [ "$cb" = "$branch" ]; then
 	echo ${E}"  You are already on branch \`${branch}\`! Aborting...  "${X}
 	exit 1
-
-# ...and hopefully the branch exists SOMEWHERE.
 elif [ ! $onlocal ] && [ ! $onremote ]; then
+	# ...and hopefully the branch exists SOMEWHERE.
 	"${gitscripts_path}"branch.sh "$branch"
+	exit
 fi
 
 
