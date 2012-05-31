4a16b1a35cb34e5618e7ed0538795ea06ee709f4
Chris Corwin <ccorwin@finishline.com>
2012-05-31 12:34:10 -0400

    (CSC---adding-getbranch-function) added a getbranch function and added alias to cfg

---
+++ getbranch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## /*
+#	@usage getbranch
+#
+#	@description
+#	This script assertains your current branch name and copies it to your clipboard.
+#	description@
+#
+#
+#	@examples
+#	1) getbranch
+#	   # Will show your current branch name and tell you that it has been copied to your clipboard.
+#	examples@
+#
+#	@dependencies
+#	functions/5000.parse_git_branch.sh
+#	dependencies@
+#
+#	@file getbranch.sh
+## */
+$loadfuncs
+
+
+echo ${X}
+
+
+
+branch=$(__parse_git_branch)
+
+echo "Your current branch is: ${B}\`${branch}\`${X}."
+echo $branch | pbcopy
+echo "It has been copied to your clipboard."
+echo
+echo ${X}
+
+exit
