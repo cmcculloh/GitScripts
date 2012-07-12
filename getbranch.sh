96abf32d3fc4ff216a259a6b5b703ae7b5681ca0
Chris Corwin <ccorwin@finishline.com>
2012-07-12 14:53:30 -0600

    (CSC---adding-getbranch-function) WIP

--- getbranch.sh
+++ getbranch.sh
@@ -28,7 +28,7 @@ echo ${X}
 branch=$(__parse_git_branch)
 
 echo "Your current branch is: ${B}\`${branch}\`${X}."
-echo -n $branch | pbcopy
+echo -n $(__parse_git_branch) | pbcopy;
 echo "It has been copied to your clipboard."
 echo
 echo ${X}
